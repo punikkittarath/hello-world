17f659acd1f7cb3ef158b47c170645b6df49260d
punikkittarath <prabhakaran.unikkittarath@harman.com>
2025-11-01 15:11:25 +0530

    Convert C Hello World to Python script

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,8 @@
-#include <stdio.h>
+#!/usr/bin/env python3
+# Simple Hello World script
 
-int main (void0
-{
-printf("Hello World\n");
-}
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
